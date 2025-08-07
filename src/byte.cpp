63f621b20b083eb3b0bc2014726d482a2615fb09
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-07 14:20:04 +0400

    .

--- src/byte.cpp
+++ src/byte.cpp
@@ -1,28 +1,6 @@
 #include "byte.hpp"
 
-int main(int argc, char *argv[]) {
-    arg(0, argv[0]);
-
-    for (int i = 1; i < argc; i++) {
-        arg(i, argv[i]);
-        yyfile = argv[i];
-
-        int fd = open(argv[i], O_RDONLY);
-        assert(fd > 0);
-        struct stat st;
-        fstat(fd, &st);
-        size_t size = st.st_size;
-        char *src = (char *)mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
-
-        assert(size > 0);
-        lexer(src, src + size);
-
-        munmap(src, st.st_size);
-        close(fd);
-        yyfile = nullptr;
-    }
-    return 0;
-}
+// @ref main @ linux.cpp
 
 void arg(int argc, char *argv) {
     fprintf(stderr, "arg[%i] = <%s>\n", argc, argv);
