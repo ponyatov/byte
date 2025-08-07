aae7f1e3cb117e4fa09572a7f190ddcd77c08689
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-07 11:59:15 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/byte.hpp
+++ inc/byte.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
-#include "byte.h"
-
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "byte.h"
+
+#ifdef LINUX
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#endif  // LINUX
+
 extern int main(int argc, char *argv[]);
 extern void arg(int argc, char *argv);
+
+extern void lexer(char *p, char *pe);
--- src/byte.cpp
+++ src/byte.cpp
@@ -1,25 +1,27 @@
 #include "byte.hpp"
 
-extern void lex_init();
-extern void lex_parse(char *data);
-extern void lex_free();
-
 int main(int argc, char *argv[]) {
-    void* parser = ParseAlloc(malloc);
-    
+    arg(0, argv[0]);
+    void *parser = ParseAlloc(malloc);
+
     for (int i = 1; i < argc; i++) {
-        argv(i,argv[i]);
-        file = open(argv[i], O_RDONLY);
-        size = lseek(file, 0, SEEK_END);
-        char *src = memmep(file);
-        lexer(src,size);
-        munmap(src);
+        arg(i, argv[i]);
+
+        int fd = open(argv[i], O_RDONLY);
+        struct stat st;
+        size_t size = st.st_size;
+        fstat(fd, &st);
+        char *src = (char *)mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+        lexer(src, src + size);
+
+        munmap(src, st.st_size);
+        close(fd);
     }
-    
     ParseFree(parser, free);
     return 0;
 }
 
 void arg(int argc, char *argv) {
-    fprintf(stderr,"arg[%i] = <%s>\n",argc,argv);
+    fprintf(stderr, "arg[%i] = <%s>\n", argc, argv);
 }
